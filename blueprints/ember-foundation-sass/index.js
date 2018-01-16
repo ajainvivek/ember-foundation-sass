61981895bcd26edded25dd59e9197ddb67cb47bc
santhoshRaju91 <santhosh.codeholic@gmail.com>
2018-01-16 19:03:51 +0530

    Changed the addon blueprint to use foundation-sites instead of foundation

--- blueprints/ember-foundation-sass/index.js
+++ blueprints/ember-foundation-sass/index.js
@@ -1,27 +1,27 @@
-var fs          = require('fs');
-var path        = require('path');
+const fs          = require('fs');
+const path        = require('path');
 
 module.exports = {
   normalizeEntityName: function() {
   },
 
   beforeInstall: function(options) {
-    return this.addBowerPackageToProject('foundation', '~5.5.0');
+    return this.addBowerPackageToProject('foundation-sites', '6.2.3');
   },
   afterInstall: function(options) {
     //copying over the foundation.scss and _settings.scss to make foundation customization easy
-    var foundationPath = path.join(process.cwd(), 'bower_components', 'foundation', 'scss');
-    var stylePath = path.join(process.cwd(), 'app', 'styles');
-    var settingsPath = path.join(foundationPath, 'foundation', '_settings.scss');
-    var mainPath = path.join(foundationPath, 'foundation.scss');
-    var _this = this;
+    let foundationPath = path.join(process.cwd(), 'bower_components', 'foundation-sites', 'scss');
+    let stylePath = path.join(process.cwd(), 'app', 'styles');
+    let settingsPath = path.join(foundationPath, 'settings', '_settings.scss');
+    let mainPath = path.join(foundationPath, 'foundation.scss');
+    let _this = this;
 
     fs.writeFileSync(path.join(stylePath, '_settings.scss'), fs.readFileSync(settingsPath));
     fs.writeFileSync(path.join(stylePath, '_foundation.scss'), fs.readFileSync(mainPath));
 
     return this.addPackagesToProject([
-      { name: 'ember-cli-sass', target: '5.2.1'},
-      { name: 'broccoli-clean-css', target: '~1.0.0' }
+      { name: 'ember-cli-sass', target: '7.1.3'},
+      { name: 'broccoli-clean-css', target: '2.0.1' }
     ]);
   }
 };
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,7 +14,7 @@ module.exports = function(defaults) {
     },
     'sassOptions': {
       'includePaths': [
-        'bower_components/foundation/scss'
+        'bower_components/foundation-sites/scss'
       ]
     }
   });
